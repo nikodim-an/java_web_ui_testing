3fccddbb0acf36581df8502518ace00296796a49
alexander_khilchenko <kh.git@mail.ru>
2022-02-05 19:40:24 +1000

    Домашняя работа №3

---
+++ HW 3/src/main/java/ru/iq_soft/Test3.java
@@ -0,0 +1,47 @@
+package ru.iq_soft; 
+/**
+ * Класс Test3
+ *
+ * @author : Хильченко А.Н
+ * @project : HW 3
+ * @date : 04.02.2022
+ * @comments : Тест-кейс №3
+ */
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Test3 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("disable-popup-blocking");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // обработка предусловия
+        driver.get("https://ribomaniya.ru/?logout=yes");
+
+        // тестовые действия
+        driver.get("https://ribomaniya.ru/");
+        String s = driver.findElement(By.xpath("//div[@class='text-nowrap pe-2']/a")).getText();
+        assert (s.equals("8 (800) 350-32-12"));     // проверка значения по тесткейсу
+
+        //результат теста
+        System.out.println("Тест №3 пройден");      // выведется только если тест не упадет и условия удовлетворят
+        // assert
+
+        // выход из браузера
+        driver.quit();
+    }
+}
