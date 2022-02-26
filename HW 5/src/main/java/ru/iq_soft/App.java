4b3d882540b04692c5f64ea07b49a94a2b77ddda
alexander_khilchenko <kh.git@mail.ru>
2022-02-26 18:06:32 +1000

    Домашняя работа № 5

---
+++ HW 5/src/main/java/ru/iq_soft/App.java
@@ -0,0 +1,13 @@
+package ru.iq_soft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
